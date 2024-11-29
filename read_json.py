e5663c4f496365a94a20dc9e0bd2062ac18b5e88
BlankyCat <mikatx2@hotmail.com>
2024-11-29 10:26:26 +0000

    Update read_json.py

--- read_json.py
+++ read_json.py
@@ -8,10 +8,8 @@ try:
     with open(fileName, 'rt', encoding='utf-8') as file:
         data = json.load(file)
 
-    print("caminho_certo/data.json")
     print(data)
-except:
-    print("caminho_errado/data.json")
+except FileNotFoundError:
     print("Ocorreu um erro!")
 finally:
     print("Processo concluído!")
